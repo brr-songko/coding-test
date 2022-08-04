7c1f45cba3bc612884c639eb9c3ab70ea2d61aab
songko <syh6837@hanmail.net>
2022-08-04 23:41:06 +0900

    BOJ_1001

---
+++ BOJ/src/n1001_A_B/Main.java
@@ -0,0 +1,23 @@
+package n1001_A_B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+//		int A = Integer.parseInt(br.readLine());
+//		int B = Integer.parseInt(br.readLine());
+//		System.out.println(A-B);
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		System.out.println(A-B);
+		
+	}
+
+}
