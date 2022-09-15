08e3267516efddcd17a777c5b1728d2c499d2a3f
brr-songko <syh6837@hanmail.net>
2022-09-15 19:37:20 +0900

    Update

--- BOJ/src/n2750_수정렬하기/Main.java
+++ BOJ/src/n2750_수정렬하기/Main.java
@@ -1,5 +1,6 @@
 package n2750_수정렬하기;
 
+//15460KB	188ms
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,11 +10,26 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N]; 
-		
-		for(int i = 0; i < N; i++) {
+		int[] arr = new int[N];
+		int temp = 0;
+
+		for (int i = 0; i < N; i++) {
 			int a = Integer.parseInt(br.readLine());
 			arr[i] = a;
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
 		}
 	}
 }
